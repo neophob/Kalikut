735a0d791348c97d3ae8153abe119bb91e40dc4c
Michael Vogt <michu@neophob.com>
2012-02-09 17:13:15 +0100

    add more data for upcomming 2nd release

--- arduinoFw/main/kalikut/kalikut.ino
+++ arduinoFw/main/kalikut/kalikut.ino
@@ -53,6 +53,19 @@ byte modulesPerLetter[TOTAL_LETTERS] = {
   5, 2, 1, 1, 5, 2, 2, 2}; //test with one strand
 // 16, 15, 10, 10, 16, 15, 11, 10}; //actual
 
+//v 2.0
+//spliter, lower: 3 lines
+//K   lower: 00,01,02,13,14,15       // upper: 03,04,05,06,07,08,09,10,11,12   total: 16 / 16
+//A   lower: 16,17,18,26,27,28,29,30 // upper: 19,20,21,22,23,24,25            total: 15 / 31
+//L   lower: 35,36,37,38,39,40       // upper: 31,32,33,34                     total: 10 / 41
+//I   lower: 46,47,48,49,50          // upper: 41,42,43,44,45                  total: 10 / 51
+//K   lower: 51,52,53,64,65,66       // upper: 54,55,56,57,58,59,60,61,62,63   total: 16 / 67
+//U   lower: 71,72,73,74,75,76,77    // upper: 67,68,69,70,78,79,80,81         total: 15 / 82
+//T   lower: 90,91,92                // upper: 82,83,84,85,86,87,88,89         total: 11 / 93
+//now lower: 93,94,95,96,97,98,99,100,101,102                                  total: 10
+
+
+
 //the sum of all modulesPerLetter must be equal to TOTAL_MODULES
 #define TOTAL_MODULES 20
 //#define TOTAL_MODULES 103
